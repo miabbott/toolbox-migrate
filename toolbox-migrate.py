107f716c5fae7e5081bb879edb861d7b638c7649
Steve Milner <smilner@redhat.com>
2021-05-13 10:16:42 -0400

    toolbox-migrate: Classify
    Moved reusable functionality into a base class which commands
    subclass from.

--- toolbox-migrate.py
+++ toolbox-migrate.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 # TODO: Implement cleanup()
-# TODO: Probably make a Class to share code
 # TODO: Better error handling during copy/install operations
 # TODO: Diff backup RPMs and currently installed RPMs
 
@@ -12,196 +11,230 @@ import re
 import rpm
 import shutil
 import subprocess
-import sys
 
+#: Default directory to backup in
 BACKUP_DIR = ".local/share/toolbox-backup/"
+#: Location to use for yum repos
 YUM_REPOS_DIR = "/etc/yum.repos.d/"
+#: Location to use for CA/Certs
 CA_CERT_DIR = "/etc/pki/ca-trust/source/anchors/"
 
-def backup(dir=None, repos=None, rpms=None, certs=None):
-    if dir is None:
-        dir = os.path.join(os.environ['HOME'], BACKUP_DIR)
 
-    backup_dir_path=os.path.expanduser(dir)
-    if not os.path.isdir(backup_dir_path):
-        logging.debug(f"Making toolbox backup directory at {backup_dir_path}")
-        os.mkdir(os.path.expanduser(backup_dir_path))
+class _Base:
 
-    # Backup everything by default; if one of the options is not None
-    # then we'll only backup that part
-    backup_all = True
-    if repos or rpms or certs :
-        backup_all = False
+    def __call__(self, dirpath=None, repos=None, rpms=None, certs=None):
+        return self._execute(dirpath, repos, rpms, certs)
 
-    if backup_all or repos:
-        fedora_re = re.compile('^fedora.*')
-        yum_repos_dir = os.listdir(YUM_REPOS_DIR)
-        yum_repo_backup_dir = os.path.join(backup_dir_path, "repos")
-        if not os.path.isdir(yum_repo_backup_dir):
-            logging.debug(f"Making yum repo backup dir at {yum_repo_backup_dir}")
-            os.mkdir(yum_repo_backup_dir)
+    def _execute(self, dirpath, repos, rpms, certs):
+        raise NotImplementedError('_execute must be implemented')
 
-        for repo in yum_repos_dir:
-            fedora_match = fedora_re.match(repo)
-            if not fedora_match:
-                src = os.path.join(YUM_REPOS_DIR, repo)
-                dst = os.path.join(yum_repo_backup_dir, repo)
-                logging.debug(f"Backing up yum repo file {repo}")
+    def require_superuser(self):
+        if os.getuid() != 0:
+            logging.error("Must run restore operation as superuser")
+            raise SystemExit(1)
+
+    def ls(self, dirpath, description):
+        dirls = os.listdir(dirpath)
+        if len(dirls) < 1:
+            logging.warning(
+                f"Did not find any {description} to restore at {dirpath}")
+            return list()
+        return dirls
+
+    def check_file_exists(self, fpath):
+        if not os.path.isfile(fpath):
+            logging.error(f"Unable to find {fpath}")
+            raise SystemExit(1)
+
+    def check_dir_exists(self, dirpath, description, create=False):
+        if not os.path.isdir(dirpath):
+            err = f"Unable to find {description} dir at {dirpath}"
+            if create:
+                logging.warning(err)
+                logging.debug(
+                    f"Making {description} dir at {dirpath}")
+                os.mkdir(os.path.expanduser(dirpath))
+            else:
+                logging.error(err)
+                raise SystemExit(1)
+
+    def run_command(self, cmd, description):
+        command = subprocess.run(
+            cmd, capture_output=True, text=True)
+        if command.returncode != 0:
+            logging.error("Failed to {description}")
+            logging.error(command.stderr)
+            raise SystemExit(1)
+        return command
+
+
+class Backup(_Base):
+
+    def _execute(self, dirpath, repos, rpms, certs):
+        if dirpath is None:
+            dirpath = os.path.join(os.environ['HOME'], BACKUP_DIR)
+
+        backup_dir_path = os.path.expanduser(dirpath)
+        self.check_dir_exists(backup_dir_path, "toolbox backup", True)
+
+        # Backup everything by default; if one of the options is not None
+        # then we'll only backup that part
+        backup_all = True
+        if repos or rpms or certs:
+            backup_all = False
+
+        if backup_all or repos:
+            fedora_re = re.compile('^fedora.*')
+            yum_repos_dir = os.listdir(YUM_REPOS_DIR)
+            yum_repo_backup_dir = os.path.join(backup_dir_path, "repos")
+            self.check_dir_exists(yum_repo_backup_dir, "yum repo", True)
+
+            for repo in yum_repos_dir:
+                fedora_match = fedora_re.match(repo)
+                if not fedora_match:
+                    src = os.path.join(YUM_REPOS_DIR, repo)
+                    dst = os.path.join(yum_repo_backup_dir, repo)
+                    logging.debug(f"Backing up yum repo file {repo}")
+                    shutil.copy(src, dst)
+
+        if backup_all or rpms:
+            rpm_backup = os.path.join(backup_dir_path, "toolbox-rpms.backup")
+            logging.debug(
+                f"Backing up names of installed RPMs to {rpm_backup}")
+            txn_set = rpm.TransactionSet()
+            rpmdb = txn_set.dbMatch()
+            with open(rpm_backup, 'w') as f:
+                for rpms in rpmdb:
+                    f.write(f"{rpms['name']} ")
+
+        if backup_all or certs:
+            cert_list = os.listdir(CA_CERT_DIR)
+            cert_backup_dir = os.path.join(backup_dir_path, "certs")
+            self.check_dir_exists(cert_backup_dir, "CA cert", True)
+
+            for cert in cert_list:
+                src = os.path.join(CA_CERT_DIR, cert)
+                dst = os.path.join(cert_backup_dir, cert)
+                logging.debug(f"Backing up CA cert {cert}")
                 shutil.copy(src, dst)
 
-    if backup_all or rpms:
-        rpm_backup = os.path.join(backup_dir_path, "toolbox-rpms.backup")
-        logging.debug(f"Backing up names of installed RPMs to {rpm_backup}")
-        txn_set = rpm.TransactionSet()
-        rpmdb = txn_set.dbMatch()
-        with open(rpm_backup, 'w') as f:
-            for rpms in rpmdb:
-                f.write(f"{rpms['name']} ")
-
-    if backup_all or certs:
-        cert_list = os.listdir(CA_CERT_DIR)
-        cert_backup_dir = os.path.join(backup_dir_path, "certs")
-        if not os.path.isdir(cert_backup_dir):
-            logging.debug(f"Making CA cert backup dir at {cert_backup_dir}")
-            os.mkdir(cert_backup_dir)
-
-        for cert in cert_list:
-            src = os.path.join(CA_CERT_DIR, cert)
-            dst = os.path.join(cert_backup_dir, cert)
-            logging.debug(f"Backing up CA cert {cert}")
-            shutil.copy(src, dst)            
-
-    logging.debug("Backup of toolbox config complete")
+        logging.debug("Backup of toolbox config complete")
 
 
-def restore(dir=None, repos=None, rpms=None, certs=None):
-    if os.getuid() != 0:
-        logging.error("Must run restore operation as superuser")
-        sys.exit(1)
+class Restore(_Base):
 
-    if dir is None:
-        dir = os.path.join("/var/home/", os.environ['SUDO_USER'], BACKUP_DIR)
-    
-    backup_dir_path=os.path.expanduser(dir)
-    if not os.path.isdir(backup_dir_path):
-        logging.error(f"Unable to find toolbox backup dir at {backup_dir_path}")
-        sys.exit(1)
+    def _execute(self, dirpath, repos, rpms, certs):
+        self.require_superuser()
 
-    restore_all = True
-    if repos or rpms or certs:
-        restore_all = False
+        if dirpath is None:
+            dirpath = os.path.join(
+                "/var/home/", os.environ['SUDO_USER'], BACKUP_DIR)
 
-    if restore_all or certs:
-        cert_backup_dir = os.path.join(backup_dir_path, "certs")
-        if not os.path.isdir(cert_backup_dir):
-            logging.error(f"Unable to find the CA cert backup dir {cert_backup_dir}")
-            sys.exit(1)
+        backup_dir_path = os.path.expanduser(dirpath)
+        self.check_dir_exists(dirpath, "toolbox", False)
 
-        backup_certs = os.listdir(cert_backup_dir)
-        if len(backup_certs) < 1:
-            logging.warning(f"Did not find any CA certs to restore at {cert_backup_dir}")
-        else:
-            for cert in backup_certs:
-                src = os.path.join(cert_backup_dir, cert)
-                dst = os.path.join(CA_CERT_DIR, cert)
-                logging.debug(f"Restoring CA cert named {cert}")
-                shutil.copy(src, dst)
+        restore_all = True
+        if repos or rpms or certs:
+            restore_all = False
 
-            update_cp = subprocess.run(['update-ca-trust'], capture_output=True, text=True)
-            if update_cp.returncode != 0:
-                logging.error("Failed to update the CA trust")
-                logging.error(update_cp.stderr)
-                sys.exit(1)
+        if restore_all or certs:
+            cert_backup_dir = os.path.join(backup_dir_path, "certs")
+            self.check_dir_exists(cert_backup_dir, "CA cert", False)
 
-    if restore_all or repos:
-        yum_repo_backup_dir = os.path.join(backup_dir_path, "repos")
-        if not os.path.isdir(yum_repo_backup_dir):
-            logging.error(f"Unable to find repo backup dir at {yum_repo_backup_dir}")
-            sys.exit(1)
+            backup_certs = self.ls(cert_backup_dir, "CA cert")
+            if backup_certs:
+                for cert in backup_certs:
+                    src = os.path.join(cert_backup_dir, cert)
+                    dst = os.path.join(CA_CERT_DIR, cert)
+                    logging.debug(f"Restoring CA cert named {cert}")
+                    shutil.copy(src, dst)
 
-        backup_repos = os.listdir(yum_repo_backup_dir)
-        if len(backup_repos) < 1:
-            logging.warning(f"Did not find any repo files in {yum_repo_backup_dir}")
-        else:
-            for repo in backup_repos:
-                src = os.path.join(yum_repo_backup_dir, repo)
-                dst = os.path.join(YUM_REPOS_DIR, repo)
-                logging.debug(f"Restoring repo file {repo}")
-                shutil.copy(src, dst)
-        
-    if restore_all or rpms:
-        rpms_backup_file = os.path.join(backup_dir_path, "toolbox-rpms.backup")
-        if not os.path.isfile(rpms_backup_file):
-            logging.error(f"Unable to find RPM backup file at {rpms_backup_file}")
-            sys.exit(1)
-    
-        with open(rpms_backup_file, 'r') as f:
-            rpms = f.read()
+                self.run_command(
+                    ['update-ca-trust'], "update the CA trust")
 
-        dnf_install = ['dnf', '-y', '--skip-broken', 'install']
-        for r in rpms.split():
-            dnf_install.append(r)
-    
-        logging.debug("Starting restore of RPMs")
-        install_cp = subprocess.run(dnf_install, capture_output=True, text=True)
-        if install_cp.returncode != 0:
-            logging.error("Failed to restore RPMs from backup list")
-            logging.error(install_cp.stderr)
-            sys.exit(1)
-    
-        nomatch_re = re.compile("^No match for argument: (.*)")
-        nomatch_rpms = []
-        for l in install_cp.stdout.split("\n"):
-            m = nomatch_re.match(l)
-            if m:
-                nomatch_rpms.append(m.group(1))
-   
-        if len(nomatch_rpms) > 0:
-            logging.debug(f"Unable to install following RPMs: {nomatch_rpms}")
-        
-        logging.debug("Finished restoring RPMs")
-        
-    if restore_all or certs:
-        cert_backup_dir = os.path.join(backup_dir_path, "certs")
-        if not os.path.isdir(cert_backup_dir):
-            logging.error(f"Unable to find the CA cert backup dir {cert_backup_dir}")
-            sys.exit(1)
-        
-        backup_certs = os.listdir(cert_backup_dir)
-        if len(backup_certs) < 1:
-            logging.warning(f"Did not find any CA certs to restore at {cert_backup_dir}")
-        else:
-            for cert in backup_certs:
-                src = os.path.join(cert_backup_dir, cert)
-                dst = os.path.join(CA_CERT_DIR, cert)
-                logging.debug(f"Restoring CA cert named {cert}")
-                shutil.copy(src, dst)
+        if restore_all or repos:
+            yum_repo_backup_dir = os.path.join(backup_dir_path, "repos")
+            self.check_dir_exists(yum_repo_backup_dir, "repo", False)
 
-            update_cp = subprocess.run(['update-ca-trust'], capture_output=True, text=True)
-            if update_cp.returncode != 0:
-                logging.error("Failed to update the CA trust")
-                logging.error(update_cp.stderr)
-                sys.exit(1)
+            backup_repos = self.ls(yum_repo_backup_dir, 'yum repo')
+            if backup_repos:
+                for repo in backup_repos:
+                    src = os.path.join(yum_repo_backup_dir, repo)
+                    dst = os.path.join(YUM_REPOS_DIR, repo)
+                    logging.debug(f"Restoring repo file {repo}")
+                    shutil.copy(src, dst)
 
-def cleanup(dir=None, repos=None, rpms=None, certs=None):
+        if restore_all or rpms:
+            rpms_backup_file = os.path.join(
+                backup_dir_path, "toolbox-rpms.backup")
+            self.check_file_exists(rpms_backup_file)
+
+            with open(rpms_backup_file, 'r') as f:
+                rpms = f.read()
+
+            dnf_install = ['dnf', '-y', '--skip-broken', 'install']
+            for r in rpms.split():
+                dnf_install.append(r)
+
+            logging.debug("Starting restore of RPMs")
+            install_cp = self.run_command(
+                dnf_install, "restore RPMS from backup list")
+
+            nomatch_re = re.compile("^No match for argument: (.*)")
+            nomatch_rpms = []
+            for line in install_cp.stdout.split("\n"):
+                m = nomatch_re.match(line)
+                if m:
+                    nomatch_rpms.append(m.group(1))
+
+            if len(nomatch_rpms) > 0:
+                logging.debug(
+                    f"Unable to install following RPMs: {nomatch_rpms}")
+
+            logging.debug("Finished restoring RPMs")
+
+        if restore_all or certs:
+            cert_backup_dir = os.path.join(backup_dir_path, "certs")
+            self.check_dir_exists(cert_backup_dir, "CA cert", False)
+
+            backup_certs = self.ls(cert_backup_dir, "CA cert")
+            if backup_certs:
+                for cert in backup_certs:
+                    src = os.path.join(cert_backup_dir, cert)
+                    dst = os.path.join(CA_CERT_DIR, cert)
+                    logging.debug(f"Restoring CA cert named {cert}")
+                    shutil.copy(src, dst)
+
+                self.run_command(
+                    ['update-ca-trust'], 'update the CA trust')
+
+
+# TODO: Implement
+class CleanUp(_Base):
     pass
 
-            
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('operation',
-        choices=['backup','cleanup','restore'], 
+    parser.add_argument(
+        'operation',
+        choices=['backup', 'cleanup', 'restore'],
         help="The operation to perform")
-    parser.add_argument('--verbose', action='store_true',
+    parser.add_argument(
+        '--verbose', action='store_true',
         help="Make the operation more talkative")
-    parser.add_argument('--dir', 
+    parser.add_argument(
+        '--dir',
         help="Specify custom directory location to use for operations")
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('--repos', action='store_true', 
+    group.add_argument(
+        '--repos', action='store_true',
         help="Only operate on yum repos")
-    group.add_argument('--rpms', action='store_true',   
+    group.add_argument(
+        '--rpms', action='store_true',
         help="Only operate on installed RPMs")
-    group.add_argument('--certs', action='store_true',
+    group.add_argument(
+        '--certs', action='store_true',
         help="Only operate on installed CA certs")
 
     args = parser.parse_args()
@@ -209,17 +242,18 @@ def main():
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
 
+    cmd = None
     if args.operation == "backup":
-        backup(dir=args.dir, repos=args.repos, 
-            rpms=args.rpms, certs=args.certs)
+        cmd = Backup()
+    elif args.operation == "restore":
+        cmd = Restore()
+    elif args.operation == "cleanup":
+        cmd = CleanUp()
 
-    if args.operation == "restore":
-        restore(dir=args.dir, repos=args.repos, 
-            rpms=args.rpms, certs=args.certs)
+    cmd(
+        dirpath=args.dir, repos=args.repos,
+        rpms=args.rpms, certs=args.certs)
 
-    if args.operation == "cleanup":
-        cleanup(dir=args.dir, repos=args.repos, 
-            rpms=args.rpms, certs=args.certs)
 
 if __name__ == "__main__":
     main()
